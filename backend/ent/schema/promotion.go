6d38be5de8f84e93f20b58e6087ebbcab85a89a4
Pichais <yougvb@gmail.com>
2020-12-16 14:12:03 +0700

    ทำ entity ระบบPromotion -  close #28

---
+++ backend/ent/schema/promotion.go
@@ -0,0 +1,20 @@
+package schema
+
+import (
+
+)
+// Promotion holds the schema definition for the Promotion entity.
+type Promotion struct {
+}
+
+// Fields of the Promotion.
+func (Promotion) Fields() []ent.Field {
+    return []ent.Field{
+    }
+}
+
+// Edges of the Promotion.
+func (Promotion) Edges() []ent.Edge {
+    return []ent.Edge{
+
+}
