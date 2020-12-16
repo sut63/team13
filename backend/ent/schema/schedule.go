2ef28720bb9b4c7d0e7746d9c6f62f4ebb8f555f
unknown <thanabodee0206@gmail.com>
2020-12-16 14:21:49 +0700

    ทำ Entity ระบบย่อย ระบบตารางเวลาของพนักงาน - close #23

---
+++ backend/ent/schema/schedule.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/field"
+	"github.com/facebook/ent/schema/edge"
+
+)
+// Schedule holds the schema definition for the Schedule entity.
+type Schedule struct {
+	ent.Schema
+}
+
+// Fields of the Schedule.
+func (Schedule) Fields() []ent.Field {
+	return []ent.Field{
+		
+	}
+}
+
+// Edges of the User.
+func (Schedule) Edges() []ent.Edge {
+	return []ent.Edge{
+
+	}
+}
