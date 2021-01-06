93aaaae6597b564927e0a8a1e8162bdb53808e58
Pichais <yougvb@gmail.com>
2021-01-06 12:56:13 +0700

    update Promotion

--- backend/ent/schema/promotion.go
+++ backend/ent/schema/promotion.go
@@ -4,17 +4,17 @@ import (
 
 )
 // Promotion holds the schema definition for the Promotion entity.
-type Promotion struct {
+type Promotion1 struct {
 }
 
 // Fields of the Promotion.
-func (Promotion) Fields() []ent.Field {
+func (Promotion1) Fields() []ent.Field {
     return []ent.Field{
     }
 }
 
 // Edges of the Promotion.
-func (Promotion) Edges() []ent.Edge {
+func (Promotion1) Edges() []ent.Edge {
     return []ent.Edge{
 
 }
