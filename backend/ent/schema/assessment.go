53bc3bb956332a907518e6600fd82f4967ec7f8a
NontakornPayusuk <rock12_rock34@Outlook.com>
2021-01-07 15:37:53 +0700

    add schema ass,pos,sa

---
+++ backend/ent/schema/assessment.go
@@ -0,0 +1,26 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/edge"
+)
+
+// Assessment holds the schema definition for the Assessment entity.
+type Assessment struct {
+	ent.Schema
+}
+
+// Fields of the Assessment.
+func (Assessment) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("assessment").NotEmpty(),
+	}
+}
+
+// Edges of the Assessment.
+func (Assessment) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("formassessment", Salary.Type),
+	}
+}
---
+++ backend/ent/schema/salary.go
@@ -0,0 +1,35 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/edge"
+)
+
+// User holds the schema definition for the Salary entity.
+type Salary struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (Salary) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("position").NotEmpty(),
+		field.Int("Salary").Positive(),
+	}
+}
+
+// Edges of the User.
+func (Salary) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("assessment", Assessment.Type).
+		Ref("formassessment").
+		Unique(),
+		edge.From("position", Position.Type).
+		Ref("formposition").
+		Unique(),
+		edge.From("employees",Employees.Type).
+		Ref("formemployee").
+		Unique(),
+	}
+}
