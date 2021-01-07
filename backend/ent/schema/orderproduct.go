9b4a82b10340e55aa7d8811a3a0481dc617b2ad5
poommin2543 <poommin2543@gmail.com>
2021-01-07 15:18:09 +0700

    set one

---
+++ backend/ent/schema/orderproduct.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	"github.com/facebookincubator/ent"
+	"github.com/facebookincubator/ent/schema/field"
+	"github.com/facebookincubator/ent/schema/edge"
+)
+
+// Orderproduct holds the schema definition for the Orderproduct entity.
+type Orderproduct struct {
+	ent.Schema
+}
+
+// Fields of the Orderproduct.
+func (Orderproduct) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("addedtime"),
+		field.Int("stock"),
+	}
+}
+
+// Edges of the Orderproduct.
+func (Orderproduct) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("product", Product.Type).Ref("products").Unique(),
+		edge.From("company", Company.Type).Ref("companys").Unique(),
+		edge.From("Typeproduct",Typeproduct.Type).Ref("Typeproducts").Unique(),
+		edge.From("managers",Manager.Type).Ref("managers").Unique(),
+	}
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/team13/app
+
+go 1.14
