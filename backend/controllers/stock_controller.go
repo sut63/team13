e7719462cccf4bf7131f15a59d4594e1b422d623
NontakornPayusuk <rock12_rock34@Outlook.com>
2021-01-08 15:51:51 +0700

    up stock

--- backend/controllers/stock_controller.go
+++ backend/controllers/stock_controller.go
@@ -1,36 +1,37 @@
 package controllers
- 
+
 import (
-   "context"
-   "fmt"
-   "strconv"
-   "time"
-   "github.com/team13/app/ent"
-   "github.com/team13/app/ent/product"
-   "github.com/team13/app/ent/zoneproduct"
-   "github.com/team13/app/ent/employee"
-   "github.com/team13/app/ent/typeproduct"
-   "github.com/team13/app/ent/stock"
-   "github.com/gin-gonic/gin"
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/team13/app/ent"
+	"github.com/team13/app/ent/employee"
+	"github.com/team13/app/ent/product"
+	"github.com/team13/app/ent/stock"
+	"github.com/team13/app/ent/typeproduct"
+	"github.com/team13/app/ent/zoneproduct"
 )
- 
+
 // StockController defines the struct for the stock controller
 type StockController struct {
-   client *ent.Client
-   router gin.IRouter
+	client *ent.Client
+	router gin.IRouter
 }
 
 // Stock defines the struct for the stock
 type Stock struct {
-	ProductID		int
-	ZoneID			int
-	EmployeeID		int
-	TypeproductID 	int
-	Priceproduct	string
-	Amount			int
-	Time			string
+	ProductID     int
+	ZoneID        int
+	EmployeeID    int
+	TypeproductID int
+	Priceproduct  string
+	Amount        int
+	Time          string
+}
 
- }
 // CreateStock handles POST requests for adding stock entities
 // @Summary Create stock
 // @Description Create stock
@@ -53,7 +54,7 @@ func (ctl *StockController) CreateStock(c *gin.Context) {
 
 	p, err := ctl.client.Product.
 		Query().
-		Where(product.IDEQ(int(obj.ID))).
+		Where(product.IDEQ(int(obj.ProductID))).
 		Only(context.Background())
 
 	if err != nil {
@@ -65,7 +66,7 @@ func (ctl *StockController) CreateStock(c *gin.Context) {
 
 	z, err := ctl.client.Zoneproduct.
 		Query().
-		Where(zoneproduct.IDEQ(int(obj.ID))).
+		Where(zoneproduct.IDEQ(int(obj.ZoneID))).
 		Only(context.Background())
 
 	if err != nil {
@@ -77,7 +78,7 @@ func (ctl *StockController) CreateStock(c *gin.Context) {
 
 	e, err := ctl.client.Employee.
 		Query().
-		Where(employee.IDEQ(int(obj.ID))).
+		Where(employee.IDEQ(int(obj.EmployeeID))).
 		Only(context.Background())
 
 	if err != nil {
@@ -89,7 +90,7 @@ func (ctl *StockController) CreateStock(c *gin.Context) {
 
 	t, err := ctl.client.Typeproduct.
 		Query().
-		Where(typeproduct.IDEQ(int(obj.ID))).
+		Where(typeproduct.IDEQ(int(obj.TypeproductID))).
 		Only(context.Background())
 
 	if err != nil {
@@ -100,7 +101,7 @@ func (ctl *StockController) CreateStock(c *gin.Context) {
 	}
 
 	times, err := time.Parse(time.RFC3339, obj.Time)
-  
+
 	s, err := ctl.client.Stock.
 		Create().
 		SetProduct(p).
@@ -117,9 +118,10 @@ func (ctl *StockController) CreateStock(c *gin.Context) {
 		})
 		return
 	}
-  
+
 	c.JSON(200, s)
- }
+}
+
 // GetStock handles GET requests to retrieve a stock entity
 // @Summary Get a stock entity by ID
 // @Description get stock by ID
@@ -139,7 +141,7 @@ func (ctl *StockController) GetStock(c *gin.Context) {
 		})
 		return
 	}
-  
+
 	u, err := ctl.client.Stock.
 		Query().
 		Where(stock.IDEQ(int(id))).
@@ -150,10 +152,10 @@ func (ctl *StockController) GetStock(c *gin.Context) {
 		})
 		return
 	}
-  
+
 	c.JSON(200, u)
- }
- 
+}
+
 // ListStock handles request to get a list of stock entities
 // @Summary List stock entities
 // @Description list stock entities
@@ -170,16 +172,20 @@ func (ctl *StockController) ListStock(c *gin.Context) {
 	limit := 10
 	if limitQuery != "" {
 		limit64, err := strconv.ParseInt(limitQuery, 10, 64)
-		if err == nil {limit = int(limit64)}
+		if err == nil {
+			limit = int(limit64)
+		}
 	}
-  
+
 	offsetQuery := c.Query("offset")
 	offset := 0
 	if offsetQuery != "" {
 		offset64, err := strconv.ParseInt(offsetQuery, 10, 64)
-		if err == nil {offset = int(offset64)}
+		if err == nil {
+			offset = int(offset64)
+		}
 	}
-  
+
 	stocks, err := ctl.client.Stock.
 		Query().
 		WithProduct().
@@ -189,14 +195,14 @@ func (ctl *StockController) ListStock(c *gin.Context) {
 		Limit(limit).
 		Offset(offset).
 		All(context.Background())
-		if err != nil {
-		c.JSON(400, gin.H{"error": err.Error(),})
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-  
+
 	c.JSON(200, stocks)
- }
- 
+}
+
 // DeleteStock handles DELETE requests to delete a stock entity
 // @Summary Delete a stock entity by ID
 // @Description get stock by ID
@@ -216,7 +222,7 @@ func (ctl *StockController) DeleteStock(c *gin.Context) {
 		})
 		return
 	}
-  
+
 	err = ctl.client.Stock.
 		DeleteOneID(int(id)).
 		Exec(context.Background())
@@ -226,12 +232,10 @@ func (ctl *StockController) DeleteStock(c *gin.Context) {
 		})
 		return
 	}
-  
+
 	c.JSON(200, gin.H{"result": fmt.Sprintf("ok deleted %v", id)})
- }
-  
- 
- 
+}
+
 // UpdateStock handles PUT requests to update a stock entity
 // @Summary Update a stock entity by ID
 // @Description update stock by ID
@@ -252,7 +256,7 @@ func (ctl *StockController) UpdateStock(c *gin.Context) {
 		})
 		return
 	}
-  
+
 	obj := ent.Stock{}
 	if err := c.ShouldBind(&obj); err != nil {
 		c.JSON(400, gin.H{
@@ -265,12 +269,13 @@ func (ctl *StockController) UpdateStock(c *gin.Context) {
 		UpdateOne(&obj).
 		Save(context.Background())
 	if err != nil {
-		c.JSON(400, gin.H{"error": "update failed",})
+		c.JSON(400, gin.H{"error": "update failed"})
 		return
 	}
-  
+
 	c.JSON(200, u)
- }
+}
+
 // NewStockController creates and registers handles for the stock controller
 func NewStockController(router gin.IRouter, client *ent.Client) *StockController {
 	uc := &StockController{
@@ -279,17 +284,17 @@ func NewStockController(router gin.IRouter, client *ent.Client) *StockController
 	}
 	uc.register()
 	return uc
- }
-  
- // InitStockController registers routes to the main engine
- func (ctl *StockController) register() {
+}
+
+// InitStockController registers routes to the main engine
+func (ctl *StockController) register() {
 	stocks := ctl.router.Group("/stocks")
-  
+
 	stocks.GET("", ctl.ListStock)
-  
+
 	// CRUD
 	stocks.POST("", ctl.CreateStock)
 	stocks.GET(":id", ctl.GetStock)
 	stocks.PUT(":id", ctl.UpdateStock)
 	stocks.DELETE(":id", ctl.DeleteStock)
- }
+}
