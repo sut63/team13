a6e902244bec12818eb3ae59f0dc5d0fad7518ca
konrawitAEK <aekpeerat2542@gmail.com>
2021-01-11 00:16:39 +0700

    update ui aek

--- frontend/plugins/welcome/src/components/Orderonline/Orderonline.tsx
+++ frontend/plugins/welcome/src/components/Orderonline/Orderonline.tsx
@@ -1,296 +1,294 @@
-import React, { FC, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Content, Header, Page, pageTheme } from '@backstage/core';
-import SaveIcon from '@material-ui/icons/Save'; // icon save
-import Swal from 'sweetalert2'; // alert
-
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import {
-  Container,
-  Grid,
-  FormControl,
-  Select,
-  InputLabel,
-  MenuItem,
-  TextField,
-  Avatar,
-  Button,
-} from '@material-ui/core';
-import { DefaultApi } from '../../services/apis'; // Api Gennerate From Command
-import { EntUser } from '../../services/models/EntUser'; // import interface User
-import { EntVideo } from '../../services/models/EntVideo'; // import interface Video
-import { EntResolution } from '../../services/models/EntResolution'; // import interface Resolution
-import { EntPlaylist } from '../../services/models/EntPlaylist'; // import interface Playlist
+  Content,
+  Header,
+  Page,
+  pageTheme,
+  ContentHeader,
+} from '@backstage/core';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import { Alert } from '@material-ui/lab';
+import { DefaultApi } from '../../api/apis';
+import { InputLabel, MenuItem, Select } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import TableCell from '@material-ui/core/TableCell';
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@material-ui/core/Box';
 
-// header css
-const HeaderCustom = {
-  minHeight: '50px',
-};
+import { EntProduct } from '../../api/models/EntProduct';
+import { EntTypeproduct } from '../../api/models/EntTypeproduct';
+import { EntPaymentchannel } from '../../api/models/EntPaymentchannel';
+import { EntCustomer } from '../../api/models/EntCustomer';
 
-// css style
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2),
-  },
-  formControl: {
-    width: 300,
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
-  },
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    width: 300,
-  },
-}));
-
-interface watchVideo {
-  playlist: number;
-  video: number;
-  resolution: number;
-  added: Date;
-  // create_by: number;
-}
-
-const Orderonline: FC<{}> = () => {
-  const classes = useStyles();
-  const http = new DefaultApi();
-
-  const [playlist_video, setPlaylistVideo] = React.useState<
-    Partial<watchVideo>
-  >({});
-
-  const [users, setUsers] = React.useState<EntUser[]>([]);
-  const [videos, setVideos] = React.useState<EntVideo[]>([]);
-  const [resolutions, setResolutions] = React.useState<EntResolution[]>([]);
-  const [playlists, setPlaylists] = React.useState<EntPlaylist[]>([]);
-
-  // alert setting
-  const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 3000,
-    timerProgressBar: true,
-    didOpen: toast => {
-      toast.addEventListener('mouseenter', Swal.stopTimer);
-      toast.addEventListener('mouseleave', Swal.resumeTimer);
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
     },
-  });
+    margin: {
+      margin: theme.spacing(2),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(2),
+    },
+    textField: {
+      width: '25ch',
+    },
+  }),
+);
 
-  const getUsers = async () => {
-    const res = await http.listUser({ limit: 10, offset: 0 });
-    setUsers(res);
-  };
+export default function Orderonline() {
+  const classes = useStyles();
+  const profile = { givenName: 'to PositionAssingment' };
+  const api = new DefaultApi();
 
-  const getPlaylist = async () => {
-    const res = await http.listPlaylist({ limit: 10, offset: 0 });
-    setPlaylists(res);
-  };
+  //const [Positionassingment, setPositionassingment] = React.useState(initialUserState);
+  const [status, setStatus] = useState(false);
+  const [alert, setAlert] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  const getVideo = async () => {
-    const res = await http.listVideo({ limit: 10, offset: 0 });
-    setVideos(res);
-  };
+  const [products, setProducts] = React.useState<EntProduct[]>([]);
+  const [typeproducts, setTypeproducts] = React.useState<EntTypeproduct[]>([]);
+  const [paymentchannels, setPaymentchannels] = React.useState<EntPaymentchannel[]>([]);
+  const [customers, setCustomers] = React.useState<EntCustomer[]>([]);
 
-  const getResolution = async () => {
-    const res = await http.listResolution({ limit: 10, offset: 0 });
-    setResolutions(res);
-  };
+  const [product, setProduct] = useState(Number);
+  const [typeduct, setTypeproduct] = useState(Number);
+  const [paymentchannel, setPaymentchannel] = useState(Number);
+  const [customer, setCustomer] = useState(Number);
+  const [stock, setScotk] = useState(Number);
+  const [addedtime, Setaddedtime] = useState(String);
 
-  // Lifecycle Hooks
+
   useEffect(() => {
-    getUsers();
-    getVideo();
-    getResolution();
-    getPlaylist();
-  }, []);
+    const getProducts = async () => {
+      const p = await api.listProduct({ limit: 10, offset: 0 });
+      setLoading(false);
+      setProducts(p);
+    };
+    getProducts();
 
-  // set data to object playlist_video
-  const handleChange = (
-    event: React.ChangeEvent<{ name?: string; value: unknown }>,
-  ) => {
-    const name = event.target.name as keyof typeof Orderonline;
-    const { value } = event.target;
-    setPlaylistVideo({ ...playlist_video, [name]: value });
-    console.log(playlist_video);
+    const getTypeproducts = async () => {
+      const d = await api.listTypeproduct({ limit: 10, offset: 0 });
+      setLoading(false);
+      setTypeproducts(d);
+    };
+    getTypeproducts();
+
+    const getPaymentchannels = async () => {
+      const pay = await api.listPaymentchannel({ limit: 10, offset: 0 });
+      setLoading(false);
+      setPaymentchannels(pay);
+    };
+    getPaymentchannels();
+
+    const getCustomers = async () => {
+        const c = await api.listCustomer({ limit: 10, offset: 0 });
+        setLoading(false);
+        setCustomers(c);
+      };
+      getCustomers();
+
+  }, [loading]);
+
+
+  const handletimeChange = (event: any) => {
+    Setaddedtime(event.target.value as string);
   };
 
-  // clear input form
-  function clear() {
-    setPlaylistVideo({});
-  }
+  const CreateOrderonline = async () => {
+    const orderonline = {
+      productid: product,
+      typeductid: typeduct,
+      paymentchannelid: paymentchannel,
+      customerid: customer,
+      stock: stock,
+      addedtime: addedtime + "00:00+07:00"
+    }
 
-  // function save data
-  function save() {
-    const apiUrl = 'http://localhost:8080/api/v1/playlist-videos';
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(playlist_video),
-    };
+    const res: any = await api.createOrderonline({ orderonline: orderonline });
+    setStatus(true);
+    if (res.id != '') {
+      setAlert(true);
+    } else {
+      setAlert(false);
+    }
+    const timer = setTimeout(() => {
+      setStatus(false);
+    }, 1000);
+  };
 
-    console.log(playlist_video); // log ดูข้อมูล สามารถ Inspect ดูข้อมูลได้ F12 เลือก Tab Console
 
-    fetch(apiUrl, requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data.status === true) {
-          clear();
-          Toast.fire({
-            icon: 'success',
-            title: 'บันทึกข้อมูลสำเร็จ',
-          });
-        } else {
-          Toast.fire({
-            icon: 'error',
-            title: 'บันทึกข้อมูลไม่สำเร็จ',
-          });
-        }
-      });
-  }
+  const product_id_handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setProduct(event.target.value as number);
+  };
+
+  const typeproduct_id_handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setTypeproduct(event.target.value as number);
+  };
+
+  const paymentchannel_id_handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setPaymentchannel(event.target.value as number);
+  };
+
+  const customer_id_handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setCustomer(event.target.value as number);
+  };
 
   return (
     <Page theme={pageTheme.home}>
-      <Header style={HeaderCustom} title={`Watch Video`}>
-        <Avatar alt="Remy Sharp" src="../../image/account.jpg" />
-        <div style={{ marginLeft: 10 }}>Tanapon Kongjaroensuk</div>
+      <Header
+        title={`Welcome ${profile.givenName || 'to PositionAssingment'}`}
+        subtitle="Select the Position you want to be in."
+      >
+
+        <Avatar>D</Avatar>
+        <Typography component="div" variant="body1">
+          <Box color="Dang@gmail.com">Dang@gmail.com</Box>
+          <Box color="secondary.main"></Box>
+        </Typography>
+
       </Header>
       <Content>
-        <Container maxWidth="sm">
-          <Grid container spacing={3}>
-            <Grid item xs={12}></Grid>
-            <Grid item xs={3}>
-              <div className={classes.paper}>วีดีโอ</div>
-            </Grid>
-            <Grid item xs={9}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel>เลือกวีดีโอ</InputLabel>
-                <Select
-                  name="video"
-                  value={playlist_video.video || ''} // (undefined || '') = ''
-                  onChange={handleChange}
-                >
-                  {videos.map(item => {
-                    return (
-                      <MenuItem key={item.id} value={item.id}>
-                        {item.name}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-            </Grid>
 
-            <Grid item xs={3}>
-              <div className={classes.paper}>เพลย์ลิสต์</div>
-            </Grid>
-            <Grid item xs={9}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel>เลือกเพลย์ลิสต์</InputLabel>
-                <Select
-                  name="playlist"
-                  value={playlist_video.playlist || ''} // (undefined || '') = ''
-                  onChange={handleChange}
-                >
-                  {playlists.map(item => {
-                    return (
-                      <MenuItem key={item.id} value={item.id}>
-                        {item.title}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-            </Grid>
+        <ContentHeader title="PositionAssingment">          
+          {status ? (
+            <div>
+              {alert ? (
+                <Alert severity="success">
+                  This is a success alert — check it out!
+                </Alert>
+              ) : (
+                  <Alert severity="warning" style={{ marginTop: 20 }}>
+                    This is a warning alert — check it out!
+                  </Alert>
+                )}
+            </div>
+          ) : null}
+        </ContentHeader>
 
-            <Grid item xs={3}>
-              <div className={classes.paper}>ความละเอียด</div>
-            </Grid>
-            <Grid item xs={9}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel>เลือกความละเอียด</InputLabel>
-                <Select
-                  name="resolution"
-                  value={playlist_video.resolution || ''} // (undefined || '') = ''
-                  onChange={handleChange}
-                >
-                  {resolutions.map(item => {
-                    return (
-                      <MenuItem key={item.id} value={item.id}>
-                        {item.value}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-            </Grid>
+        <div className={classes.root}>
+          <form noValidate autoComplete="off">
 
-            <Grid item xs={3}>
-              <div className={classes.paper}>สมาชิกระบบ</div>
-            </Grid>
-            <Grid item xs={9}>
-              <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel>เลือกสมาชิกระบบ</InputLabel>
-                <Select
-                  // value={playlist_video.create_by || ''} // (undefined || '') = ''
-                  // onChange={handleChange}
-                  name="create_by"
-                >
-                  {users.map(item => {
-                    return (
-                      <MenuItem key={item.id} value={item.id}>
-                        {item.email}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
-            </Grid>
 
-            <Grid item xs={3}>
-              <div className={classes.paper}>เวลา</div>
-            </Grid>
-            <Grid item xs={9}>
-              <form className={classes.container} noValidate>
+
+            <TableCell align="left">
+
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant="outlined"
+                style={{ marginLeft: 560, width: 600 }}
+              >
                 <TextField
-                  label="เลือกเวลา"
-                  name="added"
+                  id="DayStart"
+                  label="DayStart"
                   type="date"
-                  value={playlist_video.added || ''} // (undefined || '') = ''
+                  value={daystart}
+                  onChange={handletimeChange}
+                  //defaultValue="2020-05-24"
                   className={classes.textField}
                   InputLabelProps={{
                     shrink: true,
                   }}
-                  onChange={handleChange}
                 />
-              </form>
-            </Grid>
+              </FormControl>
 
-            <Grid item xs={3}></Grid>
-            <Grid item xs={9}>
-              <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                startIcon={<SaveIcon />}
-                onClick={save}
+
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant="outlined"
+                style={{ marginLeft: 560, width: 600 }}
+                
               >
-                บันทึกการดู
-              </Button>
-            </Grid>
-          </Grid>
-        </Container>
+                <InputLabel id="physician_id-label">Physician</InputLabel>
+                <Select
+                  labelId="physician_id-label"
+                  label="Physician"
+                  id="physician_id"
+                  value={userid}
+                  onChange={physician_id_handleChange}
+                  style={{ width: 300 }}
+                >
+                  {physicians.map((item: EntPhysician) =>
+                    <MenuItem value={item.id}>{item.eMAIL}</MenuItem>)}
+                </Select>
+              </FormControl>
+
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant="outlined"
+                style={{ marginLeft: 560, width: 600 }}
+              >
+                <InputLabel id="department_id-label">Department</InputLabel>
+                <Select
+                  labelId="department_id-label"
+                  label="Department"
+                  id="department_id"
+                  value={department}
+                  onChange={department_id_handleChange}
+                  style={{ width: 300 }}
+                >
+                  {departments.map((item: EntDepartment) =>
+                    <MenuItem value={item.id}>{item.departmentname}</MenuItem>)}
+                </Select>
+              </FormControl>
+
+              <FormControl
+                fullWidth
+                className={classes.margin}
+                variant="outlined"
+                style={{ marginLeft: 560, width: 600 }}
+              >
+                <InputLabel id="position_id-label">Position</InputLabel>
+                <Select
+                  labelId="position_id-label"
+                  label="Position"
+                  id="position_id"
+                  value={position}
+                  onChange={position_id_handleChange}
+                  style={{ width: 300 }}
+                >
+                  {positions.map((item: EntPosition) =>
+                    <MenuItem value={item.id}>{item.nameposition}</MenuItem>)}
+                </Select>
+              </FormControl>
+            </TableCell>
+            <div className={classes.margin}>
+              <TableCell align="right">
+                <Button
+                  onClick={() => {
+                    createOrderonline();
+                  }}
+                  variant="contained"
+                  color="primary"
+                  style={{ marginLeft: 545, width: 200 }}
+                >
+                  SAVE DATA
+             </Button>
+              </TableCell>
+              <TableCell align="right">
+                <Button
+                  style={{ marginLeft: 1 }}
+                  component={RouterLink}
+                  to="/main"
+                  variant="contained"
+                >
+                  Back
+             </Button>
+              </TableCell>
+            </div>
+          </form>
+        </div>
       </Content>
     </Page>
   );
-};
-
-export default Orderonline;
+}
