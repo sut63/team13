05de98255f6d94ebb14e674eb47742386e827175
poommin2543 <poommin2543@gmail.com>
2021-01-13 17:28:32 +0700

    Rename Folder orderproduct  to Orderproduct

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -2,16 +2,16 @@ import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
 import WatchVideo from './components/WatchVideo'
 import SignIn from './components/SignIn'
-import SignInManager from './components/orderproduct/SignInOrderproduct'
+import SignInManager from './components/Orderproduct/SignInOrderproduct'
 import Orderonline from './components/Orderonline'
 import Orderonlinetable from './components/Orderonline/Tableorderonline'
-import Orderproduct from './components/orderproduct'
+import Orderproduct from './components/Orderproduct'
 import Stock from './components/Stock'
 import SplitsystemManager from './components/SplitsystemManager'
 import Salary from './components/Salary'
 import SalaryTable from './components/SalaryTable'
 import Tablestock from './components/Tablestock'
-import Tableorderproduct from './components/orderproduct/Tableorderproduct'
+import Tableorderproduct from './components/Orderproduct/Tableorderproduct'
 
 export const plugin = createPlugin({
   id: 'welcome',
