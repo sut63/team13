56d4bff94ce5203f1d92e16f47af21a42151aac7
poommin2543 <poommin2543@gmail.com>
2021-01-14 00:11:28 +0700

    fix error

--- frontend/plugins/welcome/src/components/orderproduct/Orderproduct.tsx
+++ frontend/plugins/welcome/src/components/orderproduct/Orderproduct.tsx
@@ -24,8 +24,7 @@ import { EntManager } from '../../api/models/EntManager';
 //import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import ComponanceTable from '../Tableorderproduct';  
-//import { ContentHeader } from '@backstage/core';
-import ComponanceTable from './Tableorderproduct';  
+//import { ContentHeader } from '@backstage/core'; 
 import Swal from 'sweetalert2';
 import { Cookies } from './SignInOrderproduct/Cookie'
 
@@ -506,10 +505,7 @@ function save() {
         </Toolbar>
       </AppBar>
       <ComponanceTable></ComponanceTable>
-<<<<<<< HEAD
-=======
     
->>>>>>> main
     </div>
   );
  }
